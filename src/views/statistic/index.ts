f5728e80601ae1ba4ce569607e97d5f04493037b
Tom Bakker <t.bakker@odyssion.net>
2017-02-22 12:44:58 +0000

    statistic tests

--- src/views/statistic/index.ts
+++ src/views/statistic/index.ts
@@ -1,92 +1,74 @@
-import { DOMContent, VNode, IInteractiveExtraComponentSources, IInteractiveComponentSinks, isDOMContent} from "../../interfaces";
-import { Color, Float, Size} from "../../enums";
-import xs from "xstream";
-import isolate from "@cycle/isolate";
-import {div} from "@cycle/dom";
-
+import { VNode, div } from "@cycle/dom";
+import { DOMContent, isDOMContent, StyleAndContentArgs, ComponentSources, ComponentSinks } from "../../types";
+import { Color, ColorString, Float, FloatString, Size, SizeString } from "../../enums";
+import { renderPropsAndContent, runPropsAndContent} from "../../common";
 
 export namespace Statistic {
   export interface Props {
-    text?: boolean;
-    horizontal?: boolean;
-    inverted?: boolean;
-    color?: Color;
-    size?: Size;
-    float?: Float;
+    text: boolean;
+    horizontal: boolean;
+    inverted: boolean;
+    color: Color | ColorString;
+    size: Size | SizeString;
+    float: Float | FloatString;
   }
 
-  /**
-   * A statistic component to show statistics.
-   * Accepts the following properties in props$:
-   *   horizontal?: boolean - Styles the statistic to display horizontally.
-   *   inverted?: boolean - Styles the statistic for a dark background.
-   *   color?: Color - The color of the statistic.
-   *   size?: Size - The size of the statistic.
-   *   float?: Float - Where to float the statistic.
-   * Expects the following type of content in content$: {} of
-   *   value: String|VNode - The value for the statistic.
-   *   label: String|VNode - The label for the statistic.
-   *   isText?: boolean - Formats the statistic value for text based values.
-   */
-  export function run(sources: IInteractiveExtraComponentSources<Props, DOMContent, DOMContent>) : IInteractiveComponentSinks {
-    function main(sources: IInteractiveExtraComponentSources<Props, DOMContent, DOMContent>) {
-      sources.props$ = sources.props$ ? sources.props$ : xs.of({});
-      sources.content$ = sources.content$ ? sources.content$ : xs.of("");
-      sources.extras$ = sources.extras$ ? sources.extras$ : xs.of("");
+  export interface ContentObj {
+    main: DOMContent;
+    label: DOMContent;
+  }
 
-      const vTree$ = xs.combine(sources.props$, sources.content$).map(
-        ([props,content]) => render(props, content)
-      );
-      return {
-        DOM: vTree$,
-        Events: (type) => sources.DOM.select(".statistic").events(type)
-      };
+  export type StatisticArgs = StyleAndContentArgs<Props, DOMContent, ContentObj>;
+  export type StatisticSources = ComponentSources<Props, DOMContent, ContentObj>;
+
+  export function render(arg1?: StatisticArgs|Partial<Props>|DOMContent, arg2?: DOMContent) {
+    return renderPropsAndContent(statistic, isArgs, isDOMContent, arg1, arg2);
+  }
+
+  export function run(sources: StatisticSources, scope?: string): ComponentSinks {
+    return runPropsAndContent(sources, statistic, ".statistic", scope);
+  }
+
+  function statistic(args: StatisticArgs): VNode {
+    let props = args.props ? args.props : {};
+    let content = args.content ? isDOMContent(args.content) ? {main: args.content} : args.content : {main: []};
+    return div({ props: { className: getClassname(props) } }, [].concat(
+      div({ props: { className: props.text ? "text value" : "value" } }, content.main),
+      content.label ? div({ props: { className: "label" } }, content.label) : []
+    ));
+  }
+
+  function getClassname(props: Partial<Props>): string {
+    let className = "ui";
+    if (props.horizontal) {
+      className += " horizontal";
     }
-    const isolatedMain = isolate(main);
-    return isolatedMain(sources);
-  }
-
-  /**
-   * A statistic component to show statistics.
-   * Accepts the following properties:
-   *   horizontal?: boolean - Styles the statistic to display horizontally.
-   *   inverted?: boolean - Styles the statistic for a dark background.
-   *   color?: Color - The color of the statistic.
-   *   size?: Size - The size of the statistic.
-   *   float?: Float - Where to float the statistic.
-   * Expects the following type of content: {} of
-   *   value: String|VNode - The value for the statistic.
-   *   label: String|VNode - The label for the statistic.
-   *   isText?: boolean - Formats the statistic value for text based values.
-   */
-  export function render(pOrC: Props|DOMContent = {}, c:DOMContent = "", e:DOMContent = "") : VNode {
-    let props = isDOMContent(pOrC) ? {} :pOrC;
-    let content = isDOMContent(pOrC) ? pOrC : c;
-    let label = isDOMContent(pOrC) ? c : e;
-    return div({ props: { className: getClassname(props) }}, [
-      div({ props: { className: props.text ? "text value" : "value"}}, content),
-      div({ props: { className: "label" }}, label)
-    ]);
-  }
-
-  function getClassname(props: Props) : string {
-    let className = "ui ";
-    if (props.horizontal) { 
-       className += " horizontal";
-}
-    if (props.inverted) { 
-       className += " inverted";
-}
-    if (typeof(props.color) !== "undefined") { 
-       className += Color.ToClassname(props.color);
-}
-    if (typeof(props.size) !== "undefined") { 
-       className += Size.ToClassname(props.size);
-}
-    if (typeof(props.float) !== "undefined") { 
-       className += Float.ToClassname(props.float);
-}
+    if (props.text) {
+      className += " text";
+    }
+    if (props.inverted) {
+      className += " inverted";
+    }
+    if (typeof (props.color) !== "undefined") {
+      className += Color.ToClassname(props.color);
+    }
+    if (typeof (props.size) !== "undefined") {
+      className += Size.ToClassname(props.size);
+    }
+    if (typeof (props.float) !== "undefined") {
+      className += Float.ToClassname(props.float);
+    }
     className += " statistic";
     return className;
+  }
+
+  function isArgs(obj): obj is StatisticArgs {
+    return typeof(obj) !== "undefined" && (
+      typeof(obj.props) !== "undefined" ||
+      typeof(obj.content) !== "undefined" && (
+        isDOMContent(obj.content) ||
+        isDOMContent(obj.content.main) || isDOMContent(obj.content.label)
+      )
+    );
   }
 }
---
+++ test/views/statistic.ts
@@ -0,0 +1,142 @@
+import * as assert from "assert";
+import { Statistic } from "../../src";
+import { mockDOMSource, VNode } from "@cycle/dom";
+import xsAdapter from "@cycle/xstream-adapter";
+import xs from "xstream";
+
+describe("Statistic", function () {
+  describe("render", function () {
+    it("should return a basic statistic when called without any arguments", function () {
+      let statistic = Statistic.render();
+      assert.equal(statistic.data.props.className, "ui statistic");
+      assert.equal(statistic.children.length, 1);
+    });
+    it("should support argument syntax", function () {
+      let statistic = Statistic.render({ horizontal: true }, "Content");
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+      assert.equal((statistic.children[0] as VNode).text, "Content");
+      statistic = Statistic.render("Content");
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui statistic");
+      assert.equal((statistic.children[0] as VNode).text, "Content");
+      statistic = Statistic.render({ horizontal: true });
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+    });
+    it("should support verbose argument object syntax", function () {
+      let statistic = Statistic.render({
+        props: { horizontal: true },
+        content: {
+          main: "Content",
+          label: "100%"
+        }
+      });
+      assert.equal(statistic.children.length, 2);
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+      assert.equal((statistic.children[0] as VNode).text, "Content");
+      assert.equal((statistic.children[1] as VNode).text, "100%");
+      statistic = Statistic.render({
+        content: {
+          main: "Content"
+        }
+      });
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui statistic");
+      assert.equal((statistic.children[0] as VNode).text, "Content");
+      statistic = Statistic.render({
+        content: {
+          label: "100$"
+        }
+      });
+      assert.equal(statistic.children.length, 2);
+      assert.equal(statistic.data.props.className, "ui statistic");
+      assert.equal((statistic.children[1] as VNode).text, "100$");
+      statistic = Statistic.render({
+        props: { horizontal: true }
+      });
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+    });
+    it("should support shorthand argument object syntax", function () {
+      let statistic = Statistic.render({
+        props: { horizontal: true },
+        content: "Content"
+      });
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+      assert.equal((statistic.children[0] as VNode).text, "Content");
+      statistic = Statistic.render({
+        content: "Content"
+      });
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui statistic");
+      assert.equal((statistic.children[0] as VNode).text, "Content");
+      statistic = Statistic.render({
+        props: { horizontal: true }
+      });
+      assert.equal(statistic.children.length, 1);
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+    });
+    it("should support the text variation", function () {
+      let statistic = Statistic.render({ text: true });
+      assert.equal(statistic.data.props.className, "ui text statistic");
+    });
+    it("should support the horizontal variation", function () {
+      let statistic = Statistic.render({ horizontal: true });
+      assert.equal(statistic.data.props.className, "ui horizontal statistic");
+    });
+    it("should support the inverted variation", function () {
+      let statistic = Statistic.render({ inverted: true });
+      assert.equal(statistic.data.props.className, "ui inverted statistic");
+    });
+    it("should support the color enum", function () {
+      let statistic = Statistic.render({ color: "primary" });
+      assert.equal(statistic.data.props.className, "ui primaryColored statistic");
+    });
+    it("should support the size enum", function () {
+      let statistic = Statistic.render({ size: "massive" });
+      assert.equal(statistic.data.props.className, "ui massive statistic");
+    });
+    it("should support the float enum", function () {
+      let statistic = Statistic.render({ float: "right" });
+      assert.equal(statistic.data.props.className, "ui right floated statistic");
+    });
+  });
+  describe("run", function () {
+    let dom = mockDOMSource(xsAdapter, {
+      ".___statistic": {
+        ".statistic": {
+          "click": xs.of("Clicked")
+        }
+      }
+    });
+    it("should return a statistic stream", function (done) {
+      let statistic = Statistic.run({ DOM: dom });
+      statistic.DOM.addListener({
+        next: (x) => {
+          assert.equal("ui statistic", x.data.props.className);
+          done();
+        }
+      });
+    });
+    it("should return an isolated component", function (done) {
+      let statistic = Statistic.run({ DOM: dom }, "statistic");
+      statistic.DOM.addListener({
+        next: (x) => {
+          assert.equal("div.___statistic", x.sel);
+          done();
+        }
+      });
+    });
+    it("should expose events through the events function", function (done) {
+      let statistic = Statistic.run({ DOM: dom }, "statistic");
+      statistic.events("click").addListener({
+        next: (x) => {
+          assert.equal("Clicked", x);
+          done();
+        }
+      });
+    });
+  });
+});
