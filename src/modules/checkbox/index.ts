0066250522700df519dc3086f2f29fdaa3a9fd05
Tom Bakker <t.bakker@odyssion.net>
2017-02-22 13:31:24 +0000

    checkbox tests

--- src/modules/checkbox/index.ts
+++ src/modules/checkbox/index.ts
@@ -1,85 +1,69 @@
-import { DOMContent, IInteractiveComponentSources, isDOMContent} from "../../interfaces";
 import xs from "xstream";
 import isolate from "@cycle/isolate";
-import {input, label, div, VNode} from "@cycle/dom";
+import { VNode, input, label, div } from "@cycle/dom";
+import { DOMContent, isDOMContent, ContentObj, StyleAndContentArgs, ComponentSources, ValueComponentSinks } from "../../types";
+import { renderPropsAndContent, makeIsArgs } from "../../common";
 
 export namespace Checkbox {
   export interface Props {
-    readonly?: boolean;
-    checked?: boolean;
-    disabled?: boolean;
-    fitted?: boolean;
-    name?: string;
-    radio?: boolean;
-    toggle?: boolean;
-    slider?: boolean;
+    name: string;
+    readonly: boolean;
+    checked: boolean;
+    disabled: boolean;
+    fitted: boolean;
+    radio: boolean;
+    toggle: boolean;
+    slider: boolean;
   }
-  /**
-  * Creates a checkbox element on the page.
-  * Accepts the following properties in props$:
-  *   readonly?: boolean - Styles the checkbox to appear read-only.
-  *   checked?: boolean - Styles the checkbox to appear checked.
-  *   disabled?: boolean - Styles the checkbox to appear disabled.
-  *   fitted?: boolean - Styles the checkbox for tight fits with nearby content.
-  *   radio?: string - Styles the checkbox to appear like a radio button belonging to the group.
-  *   toggle?: boolean - Styles the checkbox to appear like a toggle.
-  * Expects the following type of content in content$: DOMContent
-  * @param {ComponentSources} sources The default component sources.
-  */
-  export function run(sources: IInteractiveComponentSources<Props, DOMContent>) {
-    function main(sources: IInteractiveComponentSources<Props, DOMContent>) {
+
+  export type CheckboxArgs = StyleAndContentArgs<Props, DOMContent, ContentObj<DOMContent>>;
+  export type CheckboxSources = ComponentSources<Props, DOMContent, ContentObj<DOMContent>>;
+
+  export function render(arg1?: CheckboxArgs|Partial<Props>|DOMContent, arg2?: DOMContent) : VNode {
+    return renderPropsAndContent(checkbox, makeIsArgs(isDOMContent), isDOMContent, arg1, arg2);
+  }
+  export function run(sources: CheckboxSources, scope?: string) : ValueComponentSinks<boolean> {
+    function main(sources: CheckboxSources) {
       sources.props$ = sources.props$ ? sources.props$ : xs.of({});
       sources.content$ = sources.content$ ? sources.content$ : xs.of("");
-
-      const evt = (type) => sources.DOM.select("input").events(type);
+      const evt = (type) => sources.DOM.select(".checkbox").events(type);
+      
       const clicked$ = evt("click");
       const props$ = sources.props$.remember();
       const checked$ = props$.map(props =>
         clicked$.fold((acc, evt) => (evt.srcElement as any).checked, props.checked ? true : false)
       ).flatten().remember();
-      // const renderProps$ =  xs.combine(props$, checked$).map(
-      //   ([props, checked]) => Object.assign({}, props, {checked})
-      // );
       const vTree$ = xs.combine(props$, sources.content$).map(
-        ([props, content]) => render(props, content)
+        ([props, content]) => checkbox({props, content})
       );
 
       return {
         DOM: vTree$,
-        Events: evt,
+        events: evt,
         value$: checked$
       };
     }
-    const isolatedMain = isolate(main);
+    const isolatedMain = isolate(main, scope);
     return isolatedMain(sources);
   }
 
-  /**
-  * Creates a checkbox element on the page.
-  * Accepts the following properties in props$:
-  *   readonly?: boolean - Styles the checkbox to appear read-only.
-  *   checked?: boolean - Styles the checkbox to appear checked.
-  *   disabled?: boolean - Styles the checkbox to appear disabled.
-  *   fitted?: boolean - Styles the checkbox for tight fits with nearby content.
-  *   radio?: string - Styles the checkbox to appear like a radio button belonging to the group.
-  *   toggle?: boolean - Styles the checkbox to appear like a toggle.
-  * Expects the following type of content in content$: DOMContent
-  * @param {ComponentSources} sources The default component sources.
-  */
-  export function render(pOrC: Props|DOMContent = {}, c: DOMContent = "") {
-    let props = isDOMContent(pOrC) ? {} : pOrC;
-    let content = isDOMContent(pOrC) ? pOrC : c;
-    return div({ props: {className: getClassName(props) }}, [
-      input({props: {
-        type: props.radio ? "radio" : "checkbox",
-        name:props.name, 
-        checked:props.checked, 
-        disabled:props.readonly||props.disabled
-      }}),
-      label({props: {for:props.name}}, props.fitted ? "" : content)
+
+  function checkbox(args: CheckboxArgs) {
+    let props = args.props ? args.props : {};
+    let content = args.content ? isDOMContent(args.content) ? args.content : args.content.main : [];
+    return div({ props: { className: getClassName(props) } }, [
+      input({
+        props: {
+          type: props.radio ? "radio" : "checkbox",
+          name: props.name,
+          checked: props.checked,
+          disabled: props.readonly || props.disabled
+        }
+      }),
+      label({ props: { for: props.name } }, props.fitted ? "" : content)
     ]);
   }
-  function getClassName(props: Props) : string {
+  function getClassName(props: Partial<Props>): string {
     let className = "ui";
     if (props.readonly) {
       className += " read-only";
---
+++ test/modules/checkbox.ts
@@ -0,0 +1,142 @@
+import * as assert from "assert";
+import { Checkbox } from "../../src";
+import { mockDOMSource, VNode } from "@cycle/dom";
+import xsAdapter from "@cycle/xstream-adapter";
+import xs from "xstream";
+
+describe("Checkbox", function () {
+  describe("render", function () {
+    it("should return a basic checkbox when called without any arguments", function () {
+      let checkbox = Checkbox.render();
+      assert.equal(checkbox.data.props.className, "ui checkbox");
+      assert.equal(checkbox.children.length, 2);
+    });
+    it("should support argument syntax", function () {
+      let checkbox = Checkbox.render({ toggle: true }, "Content");
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+      assert.equal((checkbox.children[1] as VNode).text, "Content");
+      checkbox = Checkbox.render("Content");
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui checkbox");
+      assert.equal((checkbox.children[1] as VNode).text, "Content");
+      checkbox = Checkbox.render({ toggle: true });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+    });
+    it("should support verbose argument object syntax", function () {
+      let checkbox = Checkbox.render({
+        props: { toggle: true },
+        content: {
+          main: "Content"
+        }
+      });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+      assert.equal((checkbox.children[1] as VNode).text, "Content");
+      checkbox = Checkbox.render({
+        content: {
+          main: "Content"
+        }
+      });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui checkbox");
+      assert.equal((checkbox.children[1] as VNode).text, "Content");
+      checkbox = Checkbox.render({
+        props: { toggle: true }
+      });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+    });
+    it("should support shorthand argument object syntax", function () {
+      let checkbox = Checkbox.render({
+        props: { toggle: true },
+        content: "Content"
+      });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+      assert.equal((checkbox.children[1] as VNode).text, "Content");
+      checkbox = Checkbox.render({
+        content: "Content"
+      });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui checkbox");
+      assert.equal((checkbox.children[1] as VNode).text, "Content");
+      checkbox = Checkbox.render({
+        props: { toggle: true }
+      });
+      assert.equal(checkbox.children.length, 2);
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+    });
+    it("should support setting the initial state of the input", function () {
+      let checkbox = Checkbox.render({ checked: true });
+      let input = checkbox.children[0] as VNode;
+      assert.equal(input.data.props.checked, true);
+    });
+    it("should support setting the name of the input", function () {
+      let checkbox = Checkbox.render({ name: "group" });
+      let input = checkbox.children[0] as VNode;
+      assert.equal(input.data.props.name, "group");
+    });
+    it("should support the readonly state", function () {
+      let checkbox = Checkbox.render({ readonly: true });
+      assert.equal(checkbox.data.props.className, "ui read-only checkbox");
+    });
+    it("should support the disabled state", function () {
+      let checkbox = Checkbox.render({ disabled: true });
+      assert.equal(checkbox.data.props.className, "ui disabled checkbox");
+    });
+    it("should support the fitted variation", function () {
+      let checkbox = Checkbox.render({ fitted: true });
+      assert.equal(checkbox.data.props.className, "ui fitted checkbox");
+    });
+    it("should support the radio variation", function () {
+      let checkbox = Checkbox.render({ radio: true });
+      assert.equal(checkbox.data.props.className, "ui radio checkbox");
+    });
+    it("should support the toggle variation", function () {
+      let checkbox = Checkbox.render({ toggle: true });
+      assert.equal(checkbox.data.props.className, "ui toggle checkbox");
+    });
+    it("should support the slider variation", function () {
+      let checkbox = Checkbox.render({ slider: true });
+      assert.equal(checkbox.data.props.className, "ui slider checkbox");
+    });
+  });
+  describe("run", function () {
+    let dom = mockDOMSource(xsAdapter, {
+      ".___checkbox": {
+        ".checkbox": {
+          "click": xs.of("Clicked")
+        }
+      }
+    });
+    it("should return a checkbox stream", function (done) {
+      let checkbox = Checkbox.run({ DOM: dom });
+      checkbox.DOM.addListener({
+        next: (x) => {
+          assert.equal("ui checkbox", x.data.props.className);
+          done();
+        }
+      });
+    });
+    it("should return an isolated component", function (done) {
+      let checkbox = Checkbox.run({ DOM: dom }, "checkbox");
+      checkbox.DOM.addListener({
+        next: (x) => {
+          assert.equal("div.___checkbox", x.sel);
+          done();
+        }
+      });
+    });
+    it("should expose events through the events function", function (done) {
+      let checkbox = Checkbox.run({ DOM: dom }, "checkbox");
+      checkbox.events("click").addListener({
+        next: (x) => {
+          assert.equal("Clicked", x);
+          done();
+        }
+      });
+    });
+  });
+});
