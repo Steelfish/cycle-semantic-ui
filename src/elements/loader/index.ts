690aefdc805d6aff2cb89803a239ef573b8e4ae2
Tom Bakker <t.bakker@odyssion.net>
2017-02-22 09:14:26 +0100

    Loader tests

--- src/elements/loader/index.ts
+++ src/elements/loader/index.ts
@@ -1,82 +1,68 @@
-import { DOMContent, VNode, IInteractiveComponentSources, IInteractiveComponentSinks, isDOMContent } from "../../interfaces";
-import { Size } from "../../enums";
 import xs, { Stream } from "xstream";
 import isolate from "@cycle/isolate";
-import { div } from "@cycle/dom";
+import { VNode, div } from "@cycle/dom";
 import { Dimmer } from "../../modules/dimmer";
+import { DOMContent, isDOMContent, StyleAndContentArgs, ComponentSources, ComponentSinks, ContentObj } from "../../types";
+import { Size, SizeString } from "../../enums";
+import { renderPropsAndContent, makeIsArgs } from "../../common";
+import { capitalize } from "../../utils";
+
 
 export namespace Loader {
   export interface Props {
-    type: LoaderType;
-    element?: Stream<VNode>;
-    on$?: Stream<boolean>;
-    centered?: boolean;
-    active?: boolean;
-    disabled?: boolean;
-    indeterminate?: boolean;
-    inverted?: boolean;
-    text?: boolean;
-    size?: Size;
+    type: LoaderType | LoaderTypeString;
+    centered: boolean;
+    active: boolean;
+    disabled: boolean;
+    indeterminate: boolean;
+    inverted: boolean;
+    text: boolean;
+    size: Size | SizeString;
   }
-  /**
-   * A loader component to show that certain content or a page is loading.
-   * Accepts the following properties in props$:
-   *   type: LoaderType - The type of loader to use. See exported enum.
-   *   element?: Stream<VNode> - The element to mark as loaded. (Only used for type.Content)
-   *   on$: Stream<boolean> - When to show/hide the loader. (Not used for inline, add disabled property instead.)
-   *   centered?: boolean - Centers the loader spinner in its parent component.
-   *   active?: boolean - Always shows the loader.
-   *   disabled?: boolean - Always hides the loader.
-   *   indeterminate?: boolean - Makes the loader spin indicate its unsure of how long a task will take.
-   *   text?: boolean - Positions the loader to leave space for text.
-   *   size?: Size - The size of the loader.
-   * Expects the following type of content in content$ of: DOMContent
-   */
-  export function run(sources: IInteractiveComponentSources<Props, DOMContent>): IInteractiveComponentSinks {
-    function main(sources: IInteractiveComponentSources<Props, DOMContent>) {
-      sources.props$ = sources.props$ ? sources.props$ : xs.of({ type: LoaderType.Inline });
-      sources.content$ = sources.content$ ? sources.content$ : xs.of("");
 
+  export type LoaderArgs = StyleAndContentArgs<Props, DOMContent, ContentObj<DOMContent>>;
+
+  export interface LoaderSources extends ComponentSources<Props, DOMContent, ContentObj<DOMContent>> {
+    args?: {
+      element$?: Stream<VNode>;
+      on$?: Stream<boolean>;
+    };
+  }
+
+  export function render(arg1?: LoaderArgs | Partial<Props> | DOMContent, arg2?: DOMContent) {
+    return renderPropsAndContent(loader, makeIsArgs(isDOMContent), isDOMContent, arg1, arg2);
+  }
+
+  export function run(sources: LoaderSources, scope?: string): ComponentSinks {
+    function main(sources: LoaderSources) {
+      sources.props$ = sources.props$ ? sources.props$ : xs.of({ type: LoaderType.Page });
+      sources.content$ = sources.content$ ? sources.content$ : xs.of(undefined);
+
+      const on$ = sources.args && sources.args.on$ ? sources.args.on$ : xs.of(true);
       const props$ = sources.props$.remember();
       const vTree$ = xs.combine(props$, sources.content$)
-        .map(([props, content]) => render(props, content)
-        );
-      const on$ = props$.map(props => props.on$).flatten();
-      const target$ = props$.map(props => props.type === LoaderType.Page ? xs.of("page") : props.element).flatten();
+        .map(([props, content]) => [loader({ props, content })]
+      );
+      const target$ = props$.map(props => props.type === LoaderType.Page ? xs.of("page") : sources.args.element$).flatten();
       const dimmer = Dimmer.run({ DOM: sources.DOM, args$: on$, target$, content$: vTree$ }, props$.map(props => props.inverted));
       const result$ = props$.map(
         props => props.type === LoaderType.Inline ? vTree$ : dimmer.DOM
       ).flatten();
       return {
         DOM: result$,
-        Events: (type) => sources.DOM.select(".loader").events(type),
-        value$: xs.never()
+        events: (type) => sources.DOM.select(".loader").events(type)
       };
     }
-    const isolatedMain = isolate(main);
+    const isolatedMain = isolate(main, scope);
     return isolatedMain(sources);
   }
 
-  /**
-   * A loader component to show that certain content or a page is loading.
-   * Accepts the following properties:
-   *   type: LoaderType - The type of loader to use. See exported enum.
-   *   element?: Stream<VNode> - The element to mark as loaded. (Only used for type.Content)
-   *   on$: Stream<boolean> - When to show/hide the loader. (Not used for inline, add disabled property instead.)
-   *   centered?: boolean - Centers the loader spinner in its parent component.
-   *   active?: boolean - Always shows the loader.
-   *   disabled?: boolean - Always hides the loader.
-   *   indeterminate?: boolean - Makes the loader spin indicate its unsure of how long a task will take.
-   *   text?: boolean - Positions the loader to leave space for text.
-   *   size?: Size - The size of the loader.
-   * Expects the following type of content: DOMContent
-   */
-  export function render(pOrC: Props | DOMContent = {type: LoaderType.Page}, c: DOMContent = ""): VNode[] {
-    let props = isDOMContent(pOrC) ? { type: LoaderType.Page } : pOrC;
-    let content = isDOMContent(pOrC) ? pOrC : c;
-    return [div({ props: { className: getClassname(props) } }, content)];
+  function loader(args: LoaderArgs): VNode {
+    let props = args.props ? args.props : {};
+    let content = args.content ? isDOMContent(args.content) ? args.content : args.content.main : [];
+    return div({ props: { className: getClassname(props) } }, content);
   }
-  function getClassname(props: Props): string {
+  function getClassname(props: Partial<Props>): string {
     let className = "ui";
     if (props.active) {
       className += " active";
@@ -99,14 +85,22 @@ export namespace Loader {
     if (typeof (props.size) !== "undefined") {
       className += Size.ToClassname(props.size);
     }
-    className += LoaderType.ToClassname(props.type);
+    className += LoaderType.ToClassname(typeof(props.type) !== "undefined" ? props.type : LoaderType.Page);
     return className;
   }
+
+  export type LoaderTypeString = "inline" | "page" | "content";
   export enum LoaderType {
     Inline, Page, Content
   }
   export namespace LoaderType {
-    export function ToClassname(type: LoaderType) {
+    export function ToEnum(attachmentOrString: LoaderType | LoaderTypeString): LoaderType {
+      return typeof (attachmentOrString) === "number"
+        ? attachmentOrString
+        : LoaderType[capitalize(attachmentOrString)];
+    }
+    export function ToClassname(type: LoaderType | LoaderTypeString) {
+      type = LoaderType.ToEnum(type);
       switch (type) {
         case LoaderType.Inline: return " inline loader";
         case LoaderType.Page: return " loader";
---
+++ test/elements/loader.ts
@@ -0,0 +1,144 @@
+import * as assert from "assert";
+import { Loader } from "../../src";
+import { mockDOMSource, VNode } from "@cycle/dom";
+import xsAdapter from "@cycle/xstream-adapter";
+import xs from "xstream";
+
+describe("Loader", function () {
+  describe("render", function () {
+    it("should return a basic loader when called without any arguments", function () {
+      let loader = Loader.render();
+      assert.equal(loader.data.props.className, "ui loader");
+      assert.equal(loader.children.length, 0);
+    });
+    it("should support argument syntax", function () {
+      let loader = Loader.render({ centered: true }, ["Content"]);
+      assert.equal(loader.children.length, 1);
+      assert.equal(loader.data.props.className, "ui centered loader");
+      assert.equal((loader.children[0] as VNode).text, "Content");
+      loader = Loader.render(["Content"]);
+      assert.equal(loader.children.length, 1);
+      assert.equal(loader.data.props.className, "ui loader");
+      assert.equal((loader.children[0] as VNode).text, "Content");
+      loader = Loader.render({ centered: true });
+      assert.equal(loader.children.length, 0);
+      assert.equal(loader.data.props.className, "ui centered loader");
+    });
+    it("should support verbose argument object syntax", function () {
+      let loader = Loader.render({
+        props: { centered: true },
+        content: {
+          main: ["Content"],
+        }
+      });
+      assert.equal(loader.children.length, 1);
+      assert.equal(loader.data.props.className, "ui centered loader");
+      assert.equal((loader.children[0] as VNode).text, "Content");
+      loader = Loader.render({
+        content: {
+          main: ["Content"],
+        }
+      });
+      assert.equal(loader.children.length, 1);
+      assert.equal(loader.data.props.className, "ui loader");
+      assert.equal((loader.children[0] as VNode).text, "Content");
+      loader = Loader.render({
+        props: { centered: true }
+      });
+      assert.equal(loader.children.length, 0);
+      assert.equal(loader.data.props.className, "ui centered loader");
+    });
+    it("should support shorthand argument object syntax", function () {
+      let loader = Loader.render({
+        props: { centered: true },
+        content: ["Content"]
+      });
+      assert.equal(loader.children.length, 1);
+      assert.equal(loader.data.props.className, "ui centered loader");
+      assert.equal((loader.children[0] as VNode).text, "Content");
+      loader = Loader.render({
+        content: ["Content"]
+      });
+      assert.equal(loader.children.length, 1);
+      assert.equal(loader.data.props.className, "ui loader");
+      assert.equal((loader.children[0] as VNode).text, "Content");
+      loader = Loader.render({
+        props: { centered: true }
+      });
+      assert.equal(loader.children.length, 0);
+      assert.equal(loader.data.props.className, "ui centered loader");
+    });
+    it("should support three different types of loaders", function () {
+      let loader = Loader.render({ type: Loader.LoaderType.Content });
+      assert.equal(loader.data.props.className, "ui loader");
+      loader = Loader.render({ type: Loader.LoaderType.Page });
+      assert.equal(loader.data.props.className, "ui loader");
+      loader = Loader.render({ type: Loader.LoaderType.Inline });
+      assert.equal(loader.data.props.className, "ui inline loader");
+    });
+    it("should support the centered variation", function () {
+      let loader = Loader.render({ centered: true });
+      assert.equal(loader.data.props.className, "ui centered loader");
+    });
+    it("should support the active variation", function () {
+      let loader = Loader.render({ active: true });
+      assert.equal(loader.data.props.className, "ui active loader");
+    });
+    it("should support the disabled variation", function () {
+      let loader = Loader.render({ disabled: true });
+      assert.equal(loader.data.props.className, "ui disabled loader");
+    });
+    it("should support the indeterminate variation", function () {
+      let loader = Loader.render({ indeterminate: true });
+      assert.equal(loader.data.props.className, "ui indeterminate loader");
+    });
+    it("should support the inverted variation", function () {
+      let loader = Loader.render({ inverted: true });
+      assert.equal(loader.data.props.className, "ui inverted loader");
+    });
+    it("should support the text variation", function () {
+      let loader = Loader.render({ text: true });
+      assert.equal(loader.data.props.className, "ui text loader");
+    });
+    it("should support the size enum", function () {
+      let loader = Loader.render({ size: "massive" });
+      assert.equal(loader.data.props.className, "ui massive loader");
+    });
+  });
+  describe("run", function () {
+    let dom = mockDOMSource(xsAdapter, {
+      ".___loader": {
+        ".loader": {
+          "click": xs.of("Clicked")
+        }
+      }
+    });
+    it("should return a loader stream", function (done) {
+      let loader = Loader.run({ DOM: dom });
+      loader.DOM.addListener({
+        next: (x) => {
+          assert.equal(x.data.props.className, "ui dimmer modals page dimmer transition visible active");
+          done();
+        }
+      });
+    });
+    it("should return an isolated component", function (done) {
+      let loader = Loader.run({ DOM: dom }, "loader");
+      loader.DOM.addListener({
+        next: (x) => {
+          assert.equal(x.sel, "div.___cycle22.___loader");
+          done();
+        }
+      });
+    });
+    it("should expose events through the events function", function (done) {
+      let loader = Loader.run({ DOM: dom }, "loader");
+      loader.events("click").addListener({
+        next: (x) => {
+          assert.equal("Clicked", x);
+          done();
+        }
+      });
+    });
+  });
+});
